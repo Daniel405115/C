59ce1013e645e7234303ac7d8ba969671120a8e3
Daniel405115 <ttzk193@gmail.com>
2023-05-01 04:12:56 +0800

    nothing

--- static_variable.c
+++ static_variable.c
@@ -8,16 +8,19 @@ int main()
     foo();
     bar();
     foo();
+
     return 0;
 }
 
-void foo(void) {
+void foo( void ) 
+{
     static int i = 0; // 宣告一個靜態變數
     i++;
     printf( "foo: i = %d\n", i);
 }
 
-void bar(void) {
+void bar( void ) 
+{
     int j = 0;
     j++;
     printf( "bar: j = %d\n", j);
